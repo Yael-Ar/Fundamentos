9a37b9811e7950721fc79b144c991822d123edc1
Yael-Arce <rebollarjuan47@gmail.com>
2024-11-26 18:36:06 -0600

    Update ApoyoEstudiantes.java

--- src/u3_3101_fp_juan_yael/ApoyoEstudiantes.java
+++ src/u3_3101_fp_juan_yael/ApoyoEstudiantes.java
@@ -15,7 +15,7 @@ public class ApoyoEstudiantes {
         double ajedrez = 2500;
         double musica = 1000;
         double atletismo = 1300;
-        double porcentajeBasquetbol = (basquetbol / montoTotal) * 10;
+        double porcentajeBasquetbol = (bauetbol / montoTotal) * 10;
         double porcentajeVoleibol = (voleibol / montoTotal) * 10;
         double porcentajeArtes = (artes / montoTotal) * 10;
         double porcentajeFutbol = (futbol / montoTotal) * 10;
