f65f23578edf296d2076fe90eb56aa1b863b872e
Yael-Arce <rebollarjuan47@gmail.com>
2024-12-28 20:28:08 -0600

    Update U3_3101_FP_Juan_Yael.java

--- src/u3_3101_fp_juan_yael/U3_3101_FP_Juan_Yael.java
+++ src/u3_3101_fp_juan_yael/U3_3101_FP_Juan_Yael.java
@@ -3,9 +3,6 @@ package u3_3101_fp_juan_yael;
 
 public class U3_3101_FP_Juan_Yael {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int edad = 10
         String nombre = "Yael"
