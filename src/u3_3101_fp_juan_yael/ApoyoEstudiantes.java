18cc04de6c0ce3899db1ee8f5f89bbff5983098b
Yael-Arce <rebollarjuan47@gmail.com>
2025-01-08 21:12:35 -0600

    Update ApoyoEstudiantes.java

--- src/u3_3101_fp_juan_yael/ApoyoEstudiantes.java
+++ src/u3_3101_fp_juan_yael/ApoyoEstudiantes.java
@@ -6,14 +6,6 @@ import javax.swing.JOptionPane;
 
 public class ApoyoEstudiantes {
     public static void main(String[] args) {
-        double porcentajeBasquetbol = (bauetbol / montoTotal) * 10;
-        double porcentajeVoleibol = (voleibol / montoTotal) * 10;
-        double porcentajeArtes = (artes / montoTotal) * 10;
-        double porcentajeFutbol = (futbol / montoTotal) * 10;
-        double porcentajeTaekuondo = (taekuondo / montoTotal) * 10;
-        double porcentajeAjedrez = (ajedrez / montoTotal) * 10;
-        double porcentajeMusica = (musica / montoTotal) * 10;
-        double porcentajeAtletismo = (atletismo / montoTotal) * 10;
         String resultado = 
             "Taller: Básquetbol,  Monto asignado: $" + basquetbol + ",  Porcentaje: " + porcentajeBasquetbol + "%\n" +
             "Taller: Voleibol,  Monto asignado: $" + voleibol + ",  Porcentaje: " + porcentajeVoleibol + "%\n" +
