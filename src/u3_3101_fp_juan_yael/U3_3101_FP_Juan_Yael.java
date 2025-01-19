9836cf5899168462243de3beabfd6a13019cdd4f
Yael-Arce <rebollarjuan47@gmail.com>
2025-01-18 20:08:27 -0600

    Update U3_3101_FP_Juan_Yael.java

--- src/u3_3101_fp_juan_yael/U3_3101_FP_Juan_Yael.java
+++ src/u3_3101_fp_juan_yael/U3_3101_FP_Juan_Yael.java
@@ -6,5 +6,5 @@ public class U3_3101_FP_Juan_Yael {
     public static void main(String[] args) {
         int edad = 18
         int anioNacimiento = 2006
-        String mesNacimiento = "Noviembre"
+        
 }
